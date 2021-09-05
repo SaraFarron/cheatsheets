0c160f2a724fd87f01e237b07ab2526003eb14af
Nikita <63388801+SaraFarron@users.noreply.github.com>
2021-09-05 14:31:48 +0700

    added matplotlib samples

---
+++ python/matplotlib.py
@@ -0,0 +1,65 @@
+from matplotlib import pyplot as plt
+
+
+# The simplest way to create a plot
+
+x, y = [], []
+
+plt.plot(x, y)  # params: label
+
+# Make it beautiful
+
+plt.grid()
+plt.xlabel()
+plt.ylabel()
+plt.title()
+plt.legend()
+
+plt.show()
+
+# ax style
+
+fig, ax = plt.subplots()
+ax.plot(x, x, label='linear')
+ax.plot(x, x**2, label='quadratic')
+ax.plot(x, x**3, label='cubic')
+ax.set_xlabel('x label')
+ax.set_ylabel('y label')
+ax.set_title("Simple Plot")
+ax.legend()
+
+plt.show()
+
+# Sample of function to write DRY code
+
+def my_plotter(ax, data1, data2, param_dict):
+    """
+    A helper function to make a graph
+
+    Parameters
+    ----------
+    ax : Axes
+        The axes to draw to
+
+    data1 : array
+       The x data
+
+    data2 : array
+       The y data
+
+    param_dict : dict
+       Dictionary of kwargs to pass to ax.plot
+
+    Returns
+    -------
+    out : list
+        list of artists added
+    """
+    out = ax.plot(data1, data2, **param_dict)
+    return out
+
+# Subplots
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+my_plotter(ax1, data1, data2, {'marker': 'x'})
+my_plotter(ax2, data3, data4, {'marker': 'o'})
